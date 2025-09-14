a82113de1862daff4fe7eb5cbd47944c95c21990
Dalibor Pleskalt <c-dpleskalt@neogov.net>
2025-09-14 13:12:56 +0200

    migrate from nvim-cmp to blink.cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,92 +1,86 @@
 -- ~/.config/nvim/lua/plugins/completion.lua
--- Enhanced completion with nvim-cmp
+-- Enhanced completion with blink.cmp
 
 return {
   {
-    'hrsh7th/nvim-cmp',
+    'saghen/blink.cmp',
+    version = 'v0.*',
     event = 'InsertEnter',
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-cmdline',
       'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
       'rafamadriz/friendly-snippets',
     },
-    config = function()
-      local cmp = require('cmp')
-      local luasnip = require('luasnip')
-
-      require('luasnip.loaders.from_vscode').lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            luasnip.lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }),
-          ['<Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-          ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-            else
-              fallback()
-            end
-          end, { 'i', 's' }),
-        }),
-        sources = cmp.config.sources({
-          { name = 'nvim_lsp', priority = 1000 },
-          { name = 'luasnip', priority = 750 },
-          { name = 'buffer', priority = 500 },
-          { name = 'path', priority = 250 },
-        }),
-        performance = {
-          debounce = 60,
-          throttle = 30,
-          fetching_timeout = 500,
-          confirm_resolve_timeout = 80,
-          async_budget = 1,
-          max_view_entries = 200,
-        },
-      })
-
-      -- Command line completion
-      cmp.setup.cmdline({ '/', '?' }, {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = {
-          { name = 'buffer' }
+    opts = {
+      keymap = {
+        preset = 'default',
+        ['<C-space>'] = { 'show', 'show_documentation', 'hide_documentation' },
+        ['<C-e>'] = { 'hide' },
+        ['<CR>'] = { 'accept', 'fallback' },
+        ['<Tab>'] = { 'snippet_forward', 'select_next', 'fallback' },
+        ['<S-Tab>'] = { 'snippet_backward', 'select_prev', 'fallback' },
+        ['<C-b>'] = { 'scroll_documentation_up', 'fallback' },
+        ['<C-f>'] = { 'scroll_documentation_down', 'fallback' },
+      },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      sources = {
+        default = { 'lsp', 'path', 'snippets', 'buffer' },
+        providers = {
+          lsp = { 
+            name = 'LSP',
+            module = 'blink.cmp.sources.lsp',
+            score_offset = 100
+          },
+          path = {
+            name = 'Path',
+            module = 'blink.cmp.sources.path',
+            score_offset = 3
+          },
+          buffer = {
+            name = 'Buffer',
+            module = 'blink.cmp.sources.buffer',
+            score_offset = 5
+          }
         }
-      })
-
-      cmp.setup.cmdline(':', {
-        mapping = cmp.mapping.preset.cmdline(),
-        sources = cmp.config.sources({
-          { name = 'path' }
-        }, {
-          { name = 'cmdline' }
-        })
-      })
-    end,
+      },
+      snippets = {
+        preset = 'luasnip'
+      },
+      completion = {
+        accept = {
+          auto_brackets = {
+            enabled = true,
+          },
+        },
+        menu = {
+          border = 'rounded',
+          draw = {
+            treesitter = { 'lsp' }
+          }
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 200,
+          window = {
+            border = 'rounded'
+          }
+        },
+        ghost_text = {
+          enabled = true
+        }
+      },
+      signature = {
+        enabled = true,
+        window = {
+          border = 'rounded'
+        }
+      }
+    },
+    config = function(_, opts)
+      require('luasnip.loaders.from_vscode').lazy_load()
+      require('blink.cmp').setup(opts)
+    end
   },
 }
